751f2ce1f278b2464da3a661bccbb64f731ce0f8
Marcin Banaś <mb056625@student.ath.edu.pl>
2021-03-16 09:19:37 +0100

    poprawione

--- prog4.1/BazaDanych.cs
+++ prog4.1/BazaDanych.cs
@@ -7,7 +7,7 @@ namespace prog4._1
 {
     class BazaDanych
     {
-        public string ConnectionString { get; set; }
+        public string ConnectionString { get; set; } // czy connection string to jedyna informacja jaką mogę wyciągnąć do klasy?
         
         public BazaDanych(string connectionString)
         {
