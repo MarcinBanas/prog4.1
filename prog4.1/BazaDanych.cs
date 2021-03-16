79971b536e0465828dca10782248ca9d6e34f62e
Marcin Banaś <mb056625@student.ath.edu.pl>
2021-03-16 09:15:36 +0100

    poprawione według zaleceń

---
+++ prog4.1/BazaDanych.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace prog4._1
+{
+    class BazaDanych
+    {
+        public string ConnectionString { get; set; }
+        
+        public BazaDanych(string connectionString)
+        {
+            this.ConnectionString = connectionString;
+            
+        }
+    }
+}
--- prog4.1/Program.cs
+++ prog4.1/Program.cs
@@ -5,27 +5,9 @@ namespace prog4._1
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void DodajWpis(SqlConnection connection)
         {
-            var connectionString = @"Data Source=DESKTOP-9K2SBT4;Initial Catalog=ZNorthwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;MultipleActiveResultSets=True";
-            SqlConnection connection = new SqlConnection(connectionString);
             connection.Open();
-
-            var zapytanie = "SELECT * FROM mg.Kategorie";
-
-            var command = new SqlCommand(zapytanie, connection);
-
-            var reader = command.ExecuteReader();
-
-            while (reader.Read())
-            {
-                Console.Write(reader.GetInt32(0)+"   ");
-                Console.Write(reader.GetString(1) + "          ");
-                Console.WriteLine(reader.GetString(2) + "        ");
-                
-                
-            }
-
             var insertSql = "insert into mg.Kategorie (IDKategorii,NazwaKategorii,Opis) VALUES (@ID,@Nazwakat,@Opis)";
             var insertCommand = new SqlCommand(insertSql, connection);
             Console.WriteLine("ID:");
@@ -38,30 +20,68 @@ namespace prog4._1
             insertCommand.Parameters.Add(new SqlParameter("Nazwakat", nazwakat));
             insertCommand.Parameters.Add(new SqlParameter("Opis", opis));
             insertCommand.ExecuteNonQuery();
+            connection.Close();
+        }
 
+        public static void UsunWpis(SqlConnection connection)
+        {
+            connection.Open();
+            var insertSql3 = "delete mg.Kategorie where IDkategorii=@ID";
+            var insertCommand3 = new SqlCommand(insertSql3, connection);
+            Console.WriteLine("delete ID:");
+            var id3 = Int32.Parse(Console.ReadLine());
+
+            insertCommand3.Parameters.Add(new SqlParameter("ID", id3));
+
+            insertCommand3.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        public static void AktualizujWpis(SqlConnection connection)
+        {
+            connection.Open();
             var insertSql2 = "update mg.Kategorie set NazwaKategorii=@Nazwakat,Opis=@Opis where IDKategorii=@ID";
             var insertCommand2 = new SqlCommand(insertSql2, connection);
+
             Console.WriteLine("update ID:");
             var id2 = Int32.Parse(Console.ReadLine());
             Console.WriteLine("nowa Nazwa Kategori:");
             var nazwakat2 = Console.ReadLine();
             Console.WriteLine("nowy Opis:");
             var opis2 = Console.ReadLine();
+
             insertCommand2.Parameters.Add(new SqlParameter("ID", id2));
             insertCommand2.Parameters.Add(new SqlParameter("Nazwakat", nazwakat2));
             insertCommand2.Parameters.Add(new SqlParameter("Opis", opis2));
             insertCommand2.ExecuteNonQuery();
-
-            var insertSql3 = "delete mg.Kategorie where IDkategorii=@ID";
-            var insertCommand3 = new SqlCommand(insertSql3, connection);
-            Console.WriteLine("delete ID:");
-            var id3 = Int32.Parse(Console.ReadLine());
-            
-            insertCommand3.Parameters.Add(new SqlParameter("ID", id3));
-            
-            insertCommand3.ExecuteNonQuery();
-
             connection.Close();
+        }
+
+        public static void WyswietlDane(SqlConnection connection)
+        {
+            connection.Open();
+            var zapytanie = "SELECT * FROM mg.Kategorie";
+            var command = new SqlCommand(zapytanie, connection);
+            var reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.Write(reader.GetInt32(0) + "   ");
+                Console.Write(reader.GetString(1) + "          ");
+                Console.WriteLine(reader.GetString(2) + "        ");
+            }
+            connection.Close();
+        }
+        static void Main(string[] args)
+        {
+            BazaDanych Northwind = new BazaDanych(@"Data Source=DESKTOP-9K2SBT4;Initial Catalog=ZNorthwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;MultipleActiveResultSets=True");
+            SqlConnection connection = new SqlConnection(Northwind.ConnectionString);
+
+            WyswietlDane(connection);
+            DodajWpis(connection);
+            AktualizujWpis(connection);
+            UsunWpis(connection);
+
         }
     }
 }
